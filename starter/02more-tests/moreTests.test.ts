6f99f1e637660ea3776ff36f8cd78e5f42c92c26
sgteryn <sgteryn@gmail.com>
2022-09-23 01:45:19 -0400

    should yield correct sum

--- starter/02more-tests/moreTests.test.ts
+++ starter/02more-tests/moreTests.test.ts
@@ -21,13 +21,23 @@ it('Should summarize all number values in an array', () => {
   
       // Arrange
       // create an array of numeric string values
-      
+      const numericStringValues = ['5', '6']
       // Act
       // call the add function with the array of numeric string values
-  
+      const numericStringToNumberValues = numericStringValues.map(str => {
+        return Number(str);
+      })
+    
+      const result = add(numericStringValues)
       // Assert
       // create a variable with the expected result (sum of the array of numeric string values)
-      // use the expect function to assert that the result is equal to the expected result
+      // use the expect function to assert that the result is equal to the expected resul
+
+      const expectedResult = numericStringToNumberValues.reduce((accumulator, value) => {
+        return accumulator + value;
+      }, 0)
+
+      expect(result).toBe(expectedResult)
   })
   
   it('Should yield "NaN" if invalid number is provided', () => {
@@ -35,10 +45,12 @@ it('Should summarize all number values in an array', () => {
   
       // Arrange
       // create an array of numeric string values where one of the values is 'invalid'
-  
+      const numericStringValues = ['5a', '6']
+
       // Act
       // call the add function with the array
-  
+       const result = add(numericStringValues)
       // Assert
       // use the expect function to assert that the result is equal to NaN, use toBeNaN function
+       expect(result).toBeNaN
   });
